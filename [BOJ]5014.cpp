f7bebdc82bf6deff4c7701886d1d09e982a55b4d
bn-tw2020 <66770613+bn-tw2020@users.noreply.github.com>
2020-07-30 15:14:32 +0900

    Create [BOJ]5014.cpp

---
+++ [BOJ]5014.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int f, s, g, u, d;
+int ret=-1;
+int visited[1000004];
+queue<int> q;
+int main(){
+    cin>>f>>s>>g>>u>>d;
+    fill(visited, visited+1000004, 0);
+    q.push(s);
+    visited[s]=1;
+    if(s==g){
+      cout<<0<<'\n';
+      return 0;
+    }
+    while(q.size()){
+        int here = q.front(); q.pop();
+
+        if(here == g){
+            cout<<visited[g]-1<<"\n";
+            return 0;
+        }
+        if(!visited[here+u] && here+u<=f && here+u>=1){
+            q.push(here+u);
+            visited[here+u] = visited[here] + 1;
+        }
+        if(!visited[here-d] && here-d>=1 && here-d<=f){
+            q.push(here-d);
+            visited[here-d] = visited[here] + 1;
+        }
+      }
+    cout<<"use the stairs"<<'\n';
+    return 0;
+    }
